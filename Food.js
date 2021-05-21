6966296bc84d193497d95dec8cf2c6804afaec86
muraliseasons4 <78802546+muraliseasons4@users.noreply.github.com>
2021-05-21 11:35:41 +0530

    Add files via upload

--- Food.js
+++ Food.js
@@ -1,4 +1,4 @@
-class Food {
+class food {
   constructor(){
   this.foodStock=0;
   this.lastFed;
@@ -24,40 +24,23 @@ class Food {
   }
 
   display(){
-      background(46,139,87);
-
-      fill(255,255,254);
-      textSize(15);
-      if(lastFed>=12){
-          text("Last Feed : "+ lastFed%12 + " PM", 50,30);
-      }else if(lastFed==0){
-          text("Last Feed : 12 AM",50,30);
-      }else{
-          text("Last Feed : "+ lastFed + " AM", 50,30);
-      }
+     
       var x=70,y=100; 
       imageMode(CENTER);
+      image(this.image,720,220,70,70);
+      
       if(this.foodStock!=0){
-      for(var i=0;i<this.foodStock;i++){
-        if(i%10==0){
-          x=70;
-          y=y+50;
+        for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+            x=80;
+            y=y+50;
+          }
+          image(this.image,x,y,50,50);
+          x=x+30;
         }
-        image(this.image,x,y,50,50);
-        x=x+30;
       }
-    }
+
   }
 
-  bedroom(){
-      background(bedroom,550,500);  
-  }
-    
-  garden(){
-      background(garden,550,500);  
-  } 
-
-  washroom(){
-      background(washroom,550,500); 
-  }
+ 
 }
